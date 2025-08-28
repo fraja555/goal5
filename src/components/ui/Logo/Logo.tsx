31005e69ad9aa836f5343b41ded0330df1408042
Mbappe10 <cirereta90@gmail.com>
2025-08-28 11:06:52 +0200

    Update Logo.tsx

--- src/components/ui/Logo/Logo.tsx
+++ src/components/ui/Logo/Logo.tsx
@@ -1,5 +1,6 @@
 import cx from 'classnames'
 import { Href } from 'components/navigation'
+import VkbetLogo from '@/images/vkbet.svg' // 👈 adjust the path if needed
 
 type LogoProps = {
   className?: string
@@ -9,9 +10,9 @@ const Logo: React.FC<LogoProps> = ({ className }) => {
   return (
     <Href to="/" className={cx('flex items-center', className)}>
       <img
-        src="https://vkbet.xyz/images/vkbet.svg"
+        src={VkbetLogo}
         alt="VKBET Logo"
-        className="h-8 w-auto" // adjust height as you like
+        className="h-8 w-auto" // you can change h-8 to any Tailwind size
       />
     </Href>
   )
