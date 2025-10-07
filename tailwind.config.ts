0826a877cf7c8adf654fb76fc143763c7df8ecb3
fraja555 <fralajara333@gmail.com>
2025-10-07 11:55:37 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -68,8 +68,8 @@ const config: Config = {
         'lg': '1.25rem', // 20
       },
       backgroundImage: ({ theme }) => ({
-        'card-border-top': 'linear-gradient(180deg, rgba(50, 150, 200, 1) 0%, rgba(50, 150, 200, 1) 100%)',
-        'card-border-bottom': 'linear-gradient(180deg, rgba(50, 150, 200, 0.90) 0%, rgba(50, 150, 200, 0.90) 100%)',
+        'card-border-top': 'linear-gradient(180deg, rgba(199, 50, 50, 1) 0%, rgba(205, 55, 125, 1) 100%)',
+        'card-border-bottom': 'linear-gradient(180deg, rgba(199, 50, 50, 0.90) 0%, rgba(205, 55, 125, 0.90) 100%)',
         'live-switcher-bg': 'linear-gradient(90deg, rgba(61, 32, 31, 0.5) 0%, rgba(61, 32, 31, 0) 100%)',
         'betslip-item-bg': `linear-gradient(90.08deg, ${theme('colors.bg-l2')} 0.06%, ${theme('colors.brand-10')} 300%)`,
         'betslip-item-bg-inc': `linear-gradient(90.08deg, ${theme('colors.bg-l2')} 0.06%, ${theme('colors.accent-green')} 300%)`,
