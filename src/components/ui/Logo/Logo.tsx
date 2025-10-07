179eb3b912aacc30f696ebfe02718a95d3f3a11c
fraja555 <fralajara333@gmail.com>
2025-10-07 12:59:49 +0200

    update logo2

--- src/components/ui/Logo/Logo.tsx
+++ src/components/ui/Logo/Logo.tsx
@@ -15,7 +15,7 @@ const Logo: React.FC<LogoProps> = (props) => {
       <img
         src="/images/logo1.webp"
         alt="Logo"
-        className="h-full w-auto"
+        className="h-full w-auto max-h-full max-w-full object-contain"
       />
     </Href>
   )
